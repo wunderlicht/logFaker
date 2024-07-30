93eef821f3a80f2f4589de9d24b8ef16d1d66a64
wunderlicht <wunderlich@actualchange.eu>
2024-07-30 16:50:30 +0200

    add help

--- main.go
+++ main.go
@@ -23,23 +23,27 @@ var (
 	bofhLen     = len(BOFH)
 	logLevelLen = len(logLevel)
 	appLen      = len(app)
-	freq        int64
-	count       int64
-	stdout      bool
-	emitter     string
+	param       commandLineParameter
 )
+
+type commandLineParameter struct {
+	freq    int64
+	count   int64
+	stdout  bool
+	emitter string
+}
 
 func main() {
 	parseCmdLineArguments()
 
-	if stdout {
+	if param.stdout {
 		log.SetOutput(os.Stdout)
 	}
 
 	emitterString := emitterName()
 
-	if count > 0 {
-		for ; count > 0; count-- {
+	if param.count > 0 {
+		for ; param.count > 0; param.count-- {
 			logAndWait(emitterString())
 		}
 		return //job done
@@ -53,18 +57,25 @@ func main() {
 
 func logAndWait(emitter string) {
 	log.Printf("%s|%s|%s\n", logLevel[rand.Intn(logLevelLen)], emitter, BOFH[rand.Intn(bofhLen)])
-	time.Sleep(time.Second / time.Duration(freq))
+	time.Sleep(time.Second / time.Duration(param.freq))
 }
 
 func parseCmdLineArguments() {
-	flag.Int64Var(&freq, "freq", 2, "frequency of emitted fake lines (per second,  min. 1)")
-	flag.Int64Var(&freq, "f", 2, "short for -freq")
-	flag.Int64Var(&count, "count", 0, "number of generated fake lines (0 infinite) (default 0)")
-	flag.Int64Var(&count, "c", 0, "short for -count (default 0)")
-	flag.BoolVar(&stdout, "stdout", false, "log to stdout instead of stderr")
-	flag.StringVar(&emitter, "emitter", "", "use as emitter instead of changing it randomly")
-	flag.StringVar(&emitter, "e", "", "short for -emitter")
+	var help bool
+	flag.Int64Var(&param.freq, "freq", 2, "frequency of emitted fake lines (per second,  min. 1)")
+	flag.Int64Var(&param.freq, "f", 2, "short for -freq")
+	flag.Int64Var(&param.count, "count", 0, "number of generated fake lines (0 infinite) (default 0)")
+	flag.Int64Var(&param.count, "c", 0, "short for -count (default 0)")
+	flag.BoolVar(&param.stdout, "stdout", false, "log to stdout instead of stderr")
+	flag.StringVar(&param.emitter, "emitter", "", "use as emitter instead of changing it randomly")
+	flag.StringVar(&param.emitter, "e", "", "short for -emitter")
+	flag.BoolVar(&help, "h", false, "show help")
+	flag.BoolVar(&help, "help", false, "show help")
 	flag.Parse()
+	if help {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 }
 
 func emitterName() func() string {
@@ -72,9 +83,9 @@ func emitterName() func() string {
 	// Is it clear and easy to understand?
 	// Fun and clever does not serve the purpose here.
 	// Will be gone in the next commit.
-	if emitter != "" {
+	if param.emitter != "" {
 		return func() string {
-			return emitter
+			return param.emitter
 		}
 	}
 
